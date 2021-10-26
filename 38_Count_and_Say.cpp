c1c31d8f6357af212d9d1d0181f6a69e6edd85ff
Jacky Fang <jackyfang89@gmail.com>
2021-10-26 00:35:35 -0500

    Solved 38, pretty fast lol

--- 38_Count_and_Say.cpp
+++ 38_Count_and_Say.cpp
@@ -5,27 +5,39 @@ using namespace std;
 
 string countAndSay(int n) {
     string s = "1";
-    string ans = "1";
-    for (int i = 0; i < n; ++i) { //repetitions
-        int j = 0; 
+    for (int i = 1; i < n; ++i) {
+        int j = 0;
+        string temp = "";
         while (j < s.length()) {
-            
+            //for the current char, find consec. occurrences.
+            char curr = s[j];
+            int count = 0;
+            while (s[j] == curr) {
+                ++count;
+                ++j;
+            }
+
+            temp += count + '0';
+            temp += curr;
         }
+        s = temp;
     }
 
-    return ans;
+    return s;
 }
 
 int main() {
     vector<int> n = {
-        1, 2, 3, 4
+        1, 2, 3, 4, 5, 6
     };
 
     vector<string> ans = {
         "1", 
         "11",
         "21",
-        "1211" 
+        "1211",
+        "111221",
+        "312211"
     };
 
     for (int i = 0; i < n.size(); ++i) {
